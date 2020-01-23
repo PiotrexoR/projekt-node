1466938d43b8a6a7175a0cd175aa594ceffc3907
PiotrexoR <piotrex10439@wp.pl>
2020-01-23 15:50:18 +0100

    admin login, admin news

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 var createError = require('http-errors');
+const cookieSession = require('cookie-session');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const config = require('./config');
+const mongoose = require('mongoose');
+
+mongoose.connect(config.db, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('db connected');
+});
+
 
 var indexRouter = require('./routes/index');
 var newsRouter = require('./routes/news');
 var quizRouter = require('./routes/quiz');
 var adminRouter = require('./routes/admin');
+
 
 var app = express();
 // view engine setup
@@ -19,7 +35,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieSession({
+    name: 'session',
+    keys: config.keySession,
 
+    maxAge: config.maxAgeSession
+  }))
 app.use(function (req, res, next) {
   res.locals.path = req.path;
 
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: '',
+    keySession: ['Klucz'],
+    maxAgeSession: 24 * 60 * 60 * 1000
+}
